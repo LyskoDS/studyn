05be4e660717ff04b6748b16fb74b8c57b84a3fd
LyskoDS <lysko_dima@mail.ru>
2023-02-08 23:26:01 +0300

    Add project

---
+++ untitled/123.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+// Only change code below this line
+const wordBlanks = myNoun + ", ewfef" + myAdjective + ", vdfvdfb" + myVerb + ", sefsef" + myAdverb;
+// Only change code above this line
+console.log(wordBlanks);
