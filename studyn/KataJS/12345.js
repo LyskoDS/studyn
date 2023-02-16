f2b7947ece85a5870881f624e50cefde7754bb40
LyskoDS <lysko_dima@mail.ru>
2023-02-16 18:12:22 +0300

    ...

--- studyn/KataJS/12345.js
+++ studyn/KataJS/12345.js
@@ -1,9 +1,8 @@
-function getSumOfDigits(integer) {
-    let a = integer.split(" ").join(" ");
-    for (let i = 0; i < integer.length; i++) {
-        return
+function squareSum(numbers){
+    let summ = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        summ += (numbers[i] ** 2);
     }
-    return sum;
+    return summ;
 }
-
-console.log(getSumOfDigits(123));
+console.log(squareSum([1,2]));
